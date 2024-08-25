930d94616c3037765b687d18ba68bc96efe85cce
will-BOT-MD <ninopina10@gmail.com>
2024-08-25 15:48:52 -0600

    Actualizar menu-ia_bot.js

--- plugins/menu-ia_bot.js
+++ plugins/menu-ia_bot.js
@@ -33,7 +33,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'ai': '𝐀𝐈 𝐁𝐎𝐓',
 }
-let img = 'https://i.ibb.co/8g5yZr5/file.jpg'
+let img = 'https://telegra.ph/file/4de3155b6bd7f59b82fcd.jpg'
 
   try {
           // DEFAULT MENU
