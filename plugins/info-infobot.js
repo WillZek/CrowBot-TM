8ff679d1dceb5c58751cf681768234377a9bb4d9
will-BOT-MD <ninopina10@gmail.com>
2024-08-25 13:04:22 -0600

    Actualizar info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -81,7 +81,7 @@ yaemori += `╭─⬣「 *NodeJS Uso de memoria* 」⬣\n`
 yaemori += `${'```' + Object.keys(used).map((key, _, arr) => `│ ${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}\n`
 yaemori += `╰─⬣`
 
-await conn.reply(m.chat, yaemori, fkontak, { contextInfo: { mentionedJid: [owner[0][0] + '@s.whatsapp.net'], externalAdReply: { mediaUrl: false, mediaType: 1, description: false, title: '↷✦╎Info - Bot╎🤍˖ ⸙',body: packname, previewType: 0, thumbnail: icons, sourceUrl: redes}}})
+await conn.reply(m.chat, yaemori, fkontak, { contextInfo: { mentionedJid: [owner[0][0] + '@s.whatsapp.net'], externalAdReply: { mediaUrl: false, mediaType: 1, description: false, title: '↷✦╎Info - Bot╎💙˖ ⸙',body: packname, previewType: 0, thumbnail: icons, sourceUrl: redes}}})
 // await conn.sendFile(m.chat, imagen1, 'luffy.jpg', luffy, fkontak, null, rcanal)
 }
 handler.help = ['infobot']
