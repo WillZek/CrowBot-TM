694414e306a379cd56a2bb2fa3570b2ff4e2717a
will-BOT-MD <ninopina10@gmail.com>
2024-08-25 15:42:38 -0600

    Actualizar menu-descargas.js

--- plugins/menu-descargas.js
+++ plugins/menu-descargas.js
@@ -190,8 +190,8 @@ await m.react('📥')
         text: text,
         contextInfo: {
         externalAdReply: {
-        title: '𝐆𝐞𝐧𝐞𝐬𝐢𝐬𝐁𝐨𝐭-𝐌𝐃',
-        body: '©𝟐𝟎𝟐𝟒 𝐀𝐧𝐠𝐞𝐥𝐢𝐭𝐨-𝐎𝐅𝐂',
+        title: 'CrowBot-MD',
+        body: '©𝟐𝟎𝟐𝟒 will-BOT-MD ',
         thumbnailUrl: img,
         sourceUrl: global.canal,
         mediaType: 1,
