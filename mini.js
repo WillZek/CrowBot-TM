6b14031c41edaf9142ad7fea85359ff5d947e32a
will-BOT-MD <ninopina10@gmail.com>
2024-08-27 20:35:45 -0600

    Actualizar mini.js

--- mini.js
+++ mini.js
@@ -112,8 +112,8 @@ loadChatgptDB();
 
 /* ------------------------------------------------*/
 
-global.authFile = `GenesisSession`
-global.authFileJB = `GenesisJadiBot`
+global.authFile = `CrowSession`
+global.authFileJB = `CrowJadiBot`
 
 const {state, saveState, saveCreds} = await useMultiFileAuthState(global.authFile)
 const msgRetryCounterMap = (MessageRetryMap) => { }
